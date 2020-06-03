a19781c4ba256b223caac3b081cf373b5a11111e
Luis Fernando <luis.silva@zup.com.br>
2020-06-02 23:44:18 -0300

    Adiciona middleware para tratar erros

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const {handleError} = require('../middlewares/handleErrors');
 const AnimalController = require('../controllers/AnimalController');
 
 //API Restful
-router.get('/animals', AnimalController.findAll);
-router.get('/animals/:id', AnimalController.findById);
-router.post('/animals', AnimalController.save);
-router.patch('/animals/:id', AnimalController.update);
-router.delete('/animals/:id', AnimalController.delete);
+router.get('/test', 
+    (req, res, next) => {
+        req.body = "primeira fn";
+        next()
+    }, (req, res, next) => {
+        res.json(req.body);
+    }
+);
+router.get('/animals', handleError(AnimalController.findAll));
+router.get('/animals/:id', handleError(AnimalController.findById));
+router.post('/animals', handleError(AnimalController.save));
+router.patch('/animals/:id', handleError(AnimalController.update));
+router.delete('/animals/:id', handleError(AnimalController.delete));
 
 module.exports = router;
 
